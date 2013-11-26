2e6e29f33108d5238cd3e22d1b9ec03f9c9a48b4
Agraphie <real_freeren@yahoo.de>
2013-11-26 23:42:22 +0100

    File upload works. Parsing works. Validation works. Automatical delete of temp files works. Need to do shit now with parsed shit.

--- WholesalerWebDemo/src/de/htwg_konstanz/ebus/wholesaler/action/ImportProductCatalogAction.java
+++ WholesalerWebDemo/src/de/htwg_konstanz/ebus/wholesaler/action/ImportProductCatalogAction.java
@@ -3,10 +3,12 @@ package de.htwg_konstanz.ebus.wholesaler.action;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.nio.file.Files;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
+import javax.naming.Context;
 import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -21,7 +23,9 @@ import javax.xml.validation.SchemaFactory;
 import org.apache.commons.fileupload.FileItem;
 import org.apache.commons.fileupload.FileUploadException;
 import org.apache.commons.fileupload.disk.DiskFileItemFactory;
+import org.apache.commons.fileupload.servlet.FileCleanerCleanup;
 import org.apache.commons.fileupload.servlet.ServletFileUpload;
+import org.apache.commons.io.FileCleaningTracker;
 import org.w3c.dom.DOMImplementation;
 import org.w3c.dom.Document;
 import org.xml.sax.ErrorHandler;
@@ -54,6 +58,12 @@ public class ImportProductCatalogAction implements IAction {
 			if (Security.getInstance().isUserAllowed(loginBean.getUser(), Security.RESOURCE_ALL, Security.ACTION_READ))
 			{
 				
+				//get context from session, need to think about that
+				ServletContext context = request.getSession().getServletContext();
+				
+				//get FileCleaningTracker, tracking files saved to a temp dir
+				FileCleaningTracker fileCleaningTracker = FileCleanerCleanup.getFileCleaningTracker(context);
+					//beyond this value (in bytes) all files will be written to a temp dir
 					int yourMaxMemorySize = 1000;
 					// Create a factory for disk-based file items
 					DiskFileItemFactory factory = new DiskFileItemFactory();
@@ -61,11 +71,14 @@ public class ImportProductCatalogAction implements IAction {
 					// Set factory constraints
 					factory.setSizeThreshold(yourMaxMemorySize);
 					
+					//set FileCleaningtracker
+					factory.setFileCleaningTracker(fileCleaningTracker);
 					// Configure a repository (to ensure a secure temp location is used)
 					ServletContext servletContext = request.getSession().getServletContext();
 					File repository = (File) servletContext.getAttribute("javax.servlet.context.tempdir");
+					System.out.println(repository.getAbsolutePath());
 					factory.setRepository(repository);
-					
+
 					// Create a new file upload handler
 					ServletFileUpload upload = new ServletFileUpload(factory);
 
@@ -74,26 +87,33 @@ public class ImportProductCatalogAction implements IAction {
 					try {
 						 items = upload.parseRequest(request);
 					} catch (FileUploadException e) {
-						// TODO Auto-generated catch block
+						System.out.println("Could not parse upload request.");
 						e.printStackTrace();
 					}
 					
 					Iterator<FileItem> iter = items.iterator();
 					
+					//get and set schema
 					SchemaFactory sFactory = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
 					Schema bmeCatSchema = null;
 					try {
-						bmeCatSchema = sFactory.newSchema(new File("C:\\Users\\Simon\\Google Drive\\Wirtschaftsinformatik\\6. Semester\\E-Business Technologien\\Übung\\Aufgabenteil2\\Abgabe\\Aufgabe 2\\bmecat_new_catalog_1_2_simple_V0.96.xsd"));
+						bmeCatSchema = sFactory.newSchema(new File("C:\\temp\\bmecat_new_catalog_1_2_simple_V0.96.xsd"));
 					} catch (SAXException e1) {
 						// TODO Auto-generated catch block
+						System.out.println("could not create schema.");
 						e1.printStackTrace();
 					}
 					DocumentBuilder xmlBuilder = null;
 					DocumentBuilderFactory dbfactory = DocumentBuilderFactory.newInstance();
+					
+					//set properties
 					dbfactory.setValidating(true);
 					dbfactory.setExpandEntityReferences(true);
 					dbfactory.setIgnoringElementContentWhitespace(true);
-					dbfactory.setNamespaceAware(false);
+					
+					//this will allow namespaces to be specified in the xml documents
+					dbfactory.setNamespaceAware(true);
+					
 					dbfactory.setSchema(bmeCatSchema);
 					dbfactory.setIgnoringComments(true);
 					dbfactory.setXIncludeAware(false);
@@ -102,26 +122,40 @@ public class ImportProductCatalogAction implements IAction {
 					try {
 						xmlBuilder = dbfactory.newDocumentBuilder();
 					} catch (ParserConfigurationException e) {
-						// TODO Auto-generated catch block
+						System.out.println("Could not get xmlBuilder.");	
 						e.printStackTrace();
 					}
 					
-					xmlBuilder.setErrorHandler(null);
+					//set ErrorHandler
+					xmlBuilder.setErrorHandler(new org.xml.sax.helpers.DefaultHandler());
 					xmlBuilder.setEntityResolver(null);
 					
+					//iterate over uploaded files
 					while (iter.hasNext()) {
 					    FileItem item = iter.next();
 					    	try {
-					    		InputStream hurrdurr = item.getInputStream();
-								Document doc = xmlBuilder.parse(hurrdurr);
-								bmeCatSchema.newValidator().validate(new DOMSource(doc));
-								hurrdurr.close();
+					    		System.out.println("Memory: " + item.isInMemory());
+					    		//get input stream of the current item
+					    		InputStream uploadedItemIS = item.getInputStream();
+					    		
+					    		//parse the input stream and output a w3c document
+								Document parsedUploadedItem = xmlBuilder.parse(uploadedItemIS);
+								
+								//validate parsed file, will throw exception if not valid
+								bmeCatSchema.newValidator().validate(new DOMSource(parsedUploadedItem));
+								
+								//close input stream
+								uploadedItemIS.close();
 							} catch (SAXException e) {
-								System.out.println("is not valid");			
+								System.out.println("XML file is not valid!");			
 								e.printStackTrace();
 							} catch (IOException e) {
-								// TODO Auto-generated catch block
+								System.out.println("Could not parse to w3c document.");			
 								e.printStackTrace();
+							}finally{
+								//for deletion of create item in temp folder, otherwise gc will delete it some day
+								
+								//item.delete();
 							}
 					}
 //            // parameter not set or product not found
