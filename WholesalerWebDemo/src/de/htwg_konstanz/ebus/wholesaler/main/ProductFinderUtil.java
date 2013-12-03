d2def8ee4b582f2021b190240e5532bc8e607d46
Nnamber <simon-r@online.de>
2013-12-03 11:11:30 +0100

    productFinderUtil added

---
+++ WholesalerWebDemo/src/de/htwg_konstanz/ebus/wholesaler/main/ProductFinderUtil.java
@@ -0,0 +1,38 @@
+package de.htwg_konstanz.ebus.wholesaler.main;
+
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+
+import de.htwg_konstanz.ebus.framework.wholesaler.api.bo.BOProduct;
+import de.htwg_konstanz.ebus.framework.wholesaler.api.bo.BOSupplier;
+import de.htwg_konstanz.ebus.framework.wholesaler.api.boa.ProductBOA;
+import de.htwg_konstanz.ebus.wholesaler.demo.LoginBean;
+
+public class ProductFinderUtil {
+
+	/**
+	 *  find all available products for current Supplier
+	 * @param loginBean
+	 * @return productListTemp 
+	 */
+	public static List<BOProduct> findProductsForSupplier(LoginBean loginBean) {
+		
+		// Use temp list to save all the products which belong to the logged in supplier
+		List<BOProduct> productList = ProductBOA.getInstance().findAll();
+
+		List<BOProduct> productListTemp = new LinkedList<BOProduct>();
+		Iterator<BOProduct> iterator = productList.iterator();
+		BOProduct productTemp;
+		BOSupplier endsupplier = new Importer().supplierFinder(loginBean);
+		while (iterator.hasNext()) {
+			productTemp = iterator.next();
+			if (productTemp.getSupplier().getSupplierNumber() == endsupplier.getSupplierNumber()) {
+				productListTemp.add(productTemp);
+			}
+		}
+		return productListTemp;
+	}
+
+	
+}
